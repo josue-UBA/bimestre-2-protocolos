a472545fd232a88cc1e1a2d67ab87b445dcf4444
josueelias9 <30317390+josueelias9@users.noreply.github.com>
2021-06-17 20:44:00 -0500

    x

--- protocolos/Core/Src/main.c
+++ protocolos/Core/Src/main.c
@@ -22,7 +22,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "TJ_MPU6050.h"
+#include "stdio.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -40,18 +41,23 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
-
+RawData_Def myAccelRaw, myGyroRaw;
+ScaledData_Def myAccelScaled, myGyroScaled;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
-
+int __io_putchar(int ch);
+int _write(int file,char *ptr, int len);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -66,7 +72,7 @@ static void MX_USART2_UART_Init(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
+	MPU_ConfigTypeDef myMpuConfig;
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -88,8 +94,18 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
-
+	//1. Initialise the MPU6050 module and I2C
+	MPU6050_Init(&hi2c1);
+	//2. Configure Accel and Gyro parameters
+	myMpuConfig.Accel_Full_Scale = AFS_SEL_4g;
+	myMpuConfig.ClockSource = Internal_8MHz;
+	myMpuConfig.CONFIG_DLPF = DLPF_184A_188G_Hz;
+	myMpuConfig.Gyro_Full_Scale = FS_SEL_500;
+	myMpuConfig.Sleep_Mode_Bit = 0;  //1: sleep mode, 0: normal mode
+	MPU6050_Config(&myMpuConfig);
+		printf("holaaaa\n\r");
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -99,6 +115,18 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	  //		//Raw data
+	  //		MPU6050_Get_Accel_RawData(&myAccelRaw);
+	  //		MPU6050_Get_Gyro_RawData(&myGyroRaw);
+
+	  		//Scaled data
+	  		MPU6050_Get_Accel_Scale(&myAccelScaled);
+	  		MPU6050_Get_Gyro_Scale(&myGyroScaled);
+
+	  		HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+	  		HAL_Delay(10);
+	  		printf("holaaaa\n\r");
+
   }
   /* USER CODE END 3 */
 }
@@ -145,6 +173,40 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
 }
 
 /**
@@ -214,7 +276,23 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+int __io_putchar(int ch)
+{
+ uint8_t c[1];
+ c[0] = ch & 0x00FF;
+ HAL_UART_Transmit(&huart2, &*c, 1, 10);
+ return ch;
+}
 
+int _write(int file,char *ptr, int len)
+{
+ int DataIdx;
+ for(DataIdx= 0; DataIdx< len; DataIdx++)
+ {
+ __io_putchar(*ptr++);
+ }
+return len;
+}
 /* USER CODE END 4 */
 
 /**
